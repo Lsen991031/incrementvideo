92fbe4fb4ff0da1e3e0051841d742f3bd6213dc8
encounter1997 <1332757400@qq.com>
2023-02-17 10:48:07 +0800

    debug

--- test_ucf101.sh
+++ test_ucf101.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 main.py \
         --arch resnet34 --num_segments 8 --seed 1993 --wandb \
         --gd 20 --lr 1e-3 --lr_steps 20 30 --epochs 1 --fine_tune_epochs 1 --training --testing \
-        --train_batch-size 4 --test_batch-size 8 --exemplar_batch-size 8 -j 8 \
+        --train_batch-size 1 --test_batch-size 8 --exemplar_batch-size 8 -j 8 \
         --start_task 0 --exp 0215002 --exemplar --nme \
         --test_crops 5 --loss_type nll --store_frames uniform \
         --cl_type DIST --cl_method OURS --init_task 51 --nb_class 10 --K 40 \
